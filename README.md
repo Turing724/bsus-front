7d16126608f0c45ebb489467585e90238ee2f546
洪远麒 <415453941@qq.com>
2020-03-25 16:50:04 +0800

    remove: clear code

--- README.md
+++
@@ -1,4 +0,0 @@
-### TODO
-
-- 夜间模式
-- 分类页面(https://heckhouse.com/)
--- src/settings.js
+++
@@ -1,6 +0,0 @@
-// dev
-let isDev = /^(192\.168|localhost)/.test(window.location.host);
-
-export default {
-  host: isDev ? "http://localhost:7001" : "https://api.baishiup.com"
-};
